e5bb80913ec193629b7f5a7ef39db86287a85a03
John Doe <ee@fff.ru>
2022-10-13 09:00:45 +0300

    clinics-share fixes

--- pages/clinic-share/js/clicnic-share.js
+++ pages/clinic-share/js/clicnic-share.js
@@ -43,18 +43,18 @@ var fileTypes = [
     'image/jpeg',
     'image/pjpeg',
     'image/png'
-  ]
-  
-  function validFileType(file) {
-    for(var i = 0; i < fileTypes.length; i++) {
-      if(file.type === fileTypes[i]) {
-        return true;
-      }
+]
+
+function validFileType(file) {
+    for (var i = 0; i < fileTypes.length; i++) {
+        if (file.type === fileTypes[i]) {
+            return true;
+        }
     }
-  
+
     return false;
-  }
-  
+}
+
 function fileUpload1() {
     document.getElementById("file1").click();
 };
@@ -63,7 +63,7 @@ document.getElementById("file1").addEventListener("change", (event) => {
     let text = document.querySelector('.ui-upload-button-container.file1 button');
     text.innerHTML = document.getElementById("file1").files[0].name;
 
-    
+
 });
 
 
@@ -90,126 +90,143 @@ const visit3Container = document.querySelector(".visit3-container");
 visit2Container.style.display = "none";
 visit3Container.style.display = "none";
 
-    visit2.addEventListener("click", () => {
-        if (visit2Container.style.display === "none") {
-            visit2Container.style.display = "block";
-            visit2Container.classList.add("active-container");
-            visit2.classList.add("minus");
-          } else {
-            visit2Container.style.display = "none";
-            visit2.classList.remove("minus");
-            visit2Container.classList.remove("active-container");
-          }
-    });
-
-    visit3.addEventListener("click", () => {
-        if (visit3Container.style.display === "none") {
-            visit3Container.style.display = "block";
-            visit3.classList.add("minus");
-            visit3Container.classList.add("active-container");
-          } else {
-            visit3Container.style.display = "none";
-            visit3.classList.remove("minus");
-            visit3Container.classList.remove("active-container");
-          }
-    });
-    
-
-    function checkFields(obj) {
-        let result = true;
-    
-        function setInvalidField(field) {
-            $(field).parent().addClass('error');
-            if (result) result = false;
-        }
-    
-        function setValidField(field) {
-            $(field).parent().removeClass('error');
-        }
-    
-        obj.find('.active-container input[type="text"]').each(function () {
-            console.log($(this)[0]);
-            let id = $(this)[0].id;
-            let val = $(this).val();
-            let el = $(this);
-            $(this).removeClass('error');
-    
-            if (id === 'age-year' || id === 'male' || id === 'female' || id === 'complaints' || id === 'diagnos' || id === 'dropdown') {
-                val.length < 1 ? setInvalidField(el) : setValidField(el);
-            }
-        });
-
-        obj.find('.active-container div.ui-textarea').each(function () {
-            let id = $(this)[0].id;
-            let text = $(this)[0].innerText;
-            let el = $(this);
-            $(this).removeClass('error');
-    
-            if (id === 'anamnez' || id === 'diagnostic' || id === 'recommend' || id === 'analysis' || id === 'diagnos' || id === 'dropdown') {
-                text.length < 1 ? setInvalidField(el) : setValidField(el);
-            }
-        });
-    
-        // obj.find('#education').each(function () {
-        //     let id = $(this)[0].id;
-        //     let val = $(this).val();
-        //     let el = $(this).parent();
-        //     $(this).removeClass('error');
-        //         val == null ? setInvalidField(el) : setValidField(el);            
-        // });
-
-        // obj.find('#maritalstatus').each(function () {
-        //     let id = $(this)[0].id;
-        //     let val = $(this).val();
-        //     let el = $(this).parent();
-        //     $(this).removeClass('error');            
-        //         val == null ? setInvalidField(el) : setValidField(el);            
-        // });
-    
-        return result;
+visit2.addEventListener("click", () => {
+    if (visit2Container.style.display === "none") {
+        visit2Container.style.display = "block";
+        visit2Container.classList.add("active-container");
+        visit2.classList.add("minus");
+    } else {
+        visit2Container.style.display = "none";
+        visit2.classList.remove("minus");
+        visit2Container.classList.remove("active-container");
     }
-    
-    function submitForm(obj) {
-        if (checkFields(obj)) sendMessage(obj);
-    };
-    
-    function sendMessage(formObj) {
-        const msg = formObj.serialize();
-        const formName = formObj[0].id;
-        $.ajax({
-            type: 'POST',
-            url: '../PHPMailer.php',
-            data: msg,
-            success: function (data) {
-                if (data.status === 'success') {
-                    
-                }
-            },
-            error: function (xhr, str) {
-                doctors.style.display = 'none';
-    siteTitle.style.display = 'none';
-    content.style.display = 'none';
-    thanks.style.display = 'flex';
-    upButton.click();
-             
-                console.log('Возникла ошибка: ' + xhr.responseCode);
-            }
-        });
+});
+
+visit3.addEventListener("click", () => {
+    if (visit3Container.style.display === "none") {
+        visit3Container.style.display = "block";
+        visit3.classList.add("minus");
+        visit3Container.classList.add("active-container");
+    } else {
+        visit3Container.style.display = "none";
+        visit3.classList.remove("minus");
+        visit3Container.classList.remove("active-container");
     }
-    
-    $('body').on('submit', '#fields', function (e) {
-        e.preventDefault();
-        submitForm($('#fields'));
+});
+
+
+function checkFields(obj) {
+    let result = true;
+
+    function setInvalidField(field) {
+        $(field).parent().addClass('error');
+        if (result) result = false;
+    }
+
+    function setValidField(field) {
+        $(field).parent().removeClass('error');
+    }
+
+    obj.find('.active-container input[type="text"]').each(function () {
+        console.log($(this)[0]);
+        let id = $(this)[0].id;
+        let val = $(this).val();
+        let el = $(this);
+        $(this).removeClass('error');
+
+        numnberRegex = /^[0-9]+$/;
+
+        if (id === 'male' || id === 'female' || id === 'complaints' || id === 'diagnos' || id === 'dropdown') {
+            val.length < 1 ? setInvalidField(el) : setValidField(el);
+        }
+
+        if (id === 'age-year') {
+            numnberRegex.test(val) && val.length < 5 ? setValidField(el) : setInvalidField(el);
+        }
+
+        // if(!obj.find('#male')[0].checked && obj.find('.error').length == 0) {
+        //     result = false;
+        //     alert('Подвердите согласие на обработку')
+        // }  
+
+        // if (id === 'age-month') {
+        //     val.length == 0 || (numnberRegex.test(val) && val.length > 0 && val.length < 13 ) ? setValidField(el) : setInvalidField(el);
+        // }
     });
-    
-    $('#input[type="text"]').each(function () {
-        $(this).on("input", function () {
-            $(this).parent().removeClass('error');
-        });
+
+    obj.find('.active-container div.ui-textarea').each(function () {
+        let id = $(this)[0].id;
+        let text = $(this)[0].innerText;
+        let el = $(this);
+        $(this).removeClass('error');
+
+        if (id === 'anamnez' || id === 'diagnostic' || id === 'recommend' || id === 'analysis' || id === 'diagnos' || id === 'dropdown') {
+            text.length < 1 ? setInvalidField(el) : setValidField(el);
+        }
     });
-    
-    $('#dropdown').each(function () {
-        $(this).on("change", function () {
-            $(this).parent().parent().removeClass('error');
-        });
+
+    // obj.find('#education').each(function () {
+    //     let id = $(this)[0].id;
+    //     let val = $(this).val();
+    //     let el = $(this).parent();
+    //     $(this).removeClass('error');
+    //         val == null ? setInvalidField(el) : setValidField(el);            
+    // });
+
+    // obj.find('#maritalstatus').each(function () {
+    //     let id = $(this)[0].id;
+    //     let val = $(this).val();
+    //     let el = $(this).parent();
+    //     $(this).removeClass('error');            
+    //         val == null ? setInvalidField(el) : setValidField(el);            
+    // });
+
+    return result;
+}
+
+function submitForm(obj) {
+    if (checkFields(obj)) sendMessage(obj);
+};
+
+function sendMessage(formObj) {
+    const msg = formObj.serialize();
+    const formName = formObj[0].id;
+    $.ajax({
+        type: 'POST',
+        url: '../PHPMailer.php',
+        data: msg,
+        success: function (data) {
+            if (data.status === 'success') {
+
+            }
+        },
+        error: function (xhr, str) {
+            doctors.style.display = 'none';
+            siteTitle.style.display = 'none';
+            content.style.display = 'none';
+            thanks.style.display = 'flex';
+            upButton.click();
+
+            console.log('Возникла ошибка: ' + xhr.responseCode);
+        }
     });
+}
+
+$('body').on('submit', '#fields', function (e) {
+    e.preventDefault();
+    submitForm($('#fields'));
+});
+
+$('#input[type="text"]').each(function () {
+    $(this).on("input", function () {
+        $(this).parent().removeClass('error');
+    });
+});
+
+$('#dropdown').each(function () {
+    $(this).on("change", function () {
+        $(this).parent().parent().removeClass('error');
+    });
+});
+
+
